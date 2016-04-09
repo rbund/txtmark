1d17f46e7dd676f91ad7e4d1ea737a16264fe16a
rbund <>
2016-04-09 20:56:08 +0200

    Bugfix for issue #34: Numeric list after unnumbered lists continues unnumbered list

--- src/main/java/com/github/rjeschke/txtmark/Processor.java
+++ src/main/java/com/github/rjeschke/txtmark/Processor.java
@@ -960,8 +960,8 @@ public class Processor
                 while (line != null)
                 {
                     final LineType t = line.getLineType(this.config);
-                    if (!line.isEmpty
-                            && (line.prevEmpty && line.leading == 0 && !(t == LineType.OLIST || t == LineType.ULIST)))
+                    if (!line.isEmpty // RB: Bug fix
+                            && (line.prevEmpty && line.leading == 0 && (t != type)))/* !(t == LineType.OLIST || t == LineType.ULIST))) */
                     {
                         break;
                     }
